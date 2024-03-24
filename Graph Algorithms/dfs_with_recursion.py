33e6fe27996ba8c641d35c3c4b3f51bec42064a9
IMTIAJ HOSSAIN <emon1609bd@gmail.com>
2024-03-24 20:01:04 +0600

    Create dfs_with_recursion.py

---
+++ Graph Algorithms/dfs_with_recursion.py
@@ -0,0 +1,27 @@
+def dfs(u):
+    visited[u] = 1
+
+    for v in adj_list[u]:
+        if not visited[v]:
+            #print(f"Going from {u} to {v}")
+            dfs(v)
+
+#====================== Graph Building ==========================
+nodes = int(input())
+edges = int(input())
+
+adj_list = [[] for i in range(nodes + 1)]
+visited = [0] * int(nodes + 1)
+
+# Taking the edges
+for i in range(edges):
+    u, v = map(int, input().split(' '))
+
+    # For undirected graph
+    adj_list[u].append(v)
+    adj_list[v].append(u)
+#=================================================================
+
+visited = [0] * (nodes + 1)
+source = 1
+dfs(source)
