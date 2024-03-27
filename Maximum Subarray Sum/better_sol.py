64444fcb923c2e47909236222582d31a7de7ccab
IMTIAJ HOSSAIN <emon1609bd@gmail.com>
2024-03-27 22:57:06 +0600

    Create better_sol.py

---
+++ Maximum Subarray Sum/better_sol.py
@@ -0,0 +1,19 @@
+def maxSubArrSum(arr):
+    ans = 0
+    arr_range = []
+
+    for left in range(len(arr)):
+        sum = 0
+        for right in range(left, len(arr)):
+            sum += arr[right]
+            #ans = max(ans, sum)
+            if sum > ans:
+                ans = sum
+                arr_range = [left, right]
+
+    return ans, arr_range
+
+arr = [-2, -3, 4, -1, -2, 1, 5, -3]
+maximum_sub_array_sum, arr_range = maxSubArrSum(arr)
+print(maximum_sub_array_sum)
+print(f"The sub array is: {arr[arr_range[0] : arr_range[1]+1]}")
